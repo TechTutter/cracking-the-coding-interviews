61b6ea5cc29d8932ed55eaab96fb67b3075e69c5
TechTutter <f.lorrai.95@gmail.com>
2025-06-08 14:39:22 +0200

    chore: fix workflow

--- playground.ts
+++ playground.ts
@@ -2,11 +2,14 @@ import { Log } from "@utils/logger.ts";
 import { BitSet } from "@data-structures/bitset/BitSet.ts";
 
 function main() {
+  console.log("Running...");
   try {
     /* eslint-disable-next-line*/
     const mySet = new BitSet(16);
+
     mySet.set(1);
-    mySet.set(2);
+    console.log(mySet.get(1), mySet.get(2));
+
   } catch (error) {
     Log.error(error);
   }
